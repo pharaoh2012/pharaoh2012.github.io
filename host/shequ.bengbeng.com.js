4d6771d325b2ad6658664d7a0ffd79b30578ced7
raoming <office@raom.tk>
2014-06-06 11:18:47 +0800

    2014-06-06 周五 11:18:47.59

---
+++ host/shequ.bengbeng.com.js
@@ -0,0 +1,10 @@
+
+(function() {
+    function clickObj(o) {
+        var e = document.createEvent('MouseEvent');
+        e.initEvent('click', false, false);
+        o.dispatchEvent(e);
+    }
+
+    $('.btn_signIn').click();
+})();
