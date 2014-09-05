a418e0735b5f8a8bc1bed919747e01907887c31c
raoming <office@raom.tk>
2014-09-05 14:50:06 +0800

    2014-09-05 周五 14:50:06.46

--- host/www.wahuasuan.com.js
+++ host/www.wahuasuan.com.js
@@ -1,5 +1,6 @@
 (function() {
     function clickObj(o) {
+    	console.info("clickObj:",o)
         var e = document.createEvent('MouseEvent');
         e.initEvent('click', false, false);
         o.dispatchEvent(e);
