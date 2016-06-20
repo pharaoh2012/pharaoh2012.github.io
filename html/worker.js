80aec532bf67826ad4b8f187ff064c4fe5826310
raoming <raoming@dcqtech.com>
2016-06-20 17:29:51 +0800

    android/tool.html

--- html/worker.js
+++ html/worker.js
@@ -1,7 +1,16 @@
 // workker.js
+// addEventListener("fetch", function(evt) {
+// 	console.info(evt.request.url);
+// 	evt.respondWith(new Response("Hi, Barret Lee"));
+
+// });
+
 addEventListener("fetch", function(evt) {
-	console.info(evt.request.url);
- 	evt.respondWith(new Response("Hi, Barret Lee"));
+	evt.respondWith(new Promise(function(resolve, reject) {
+		setTimeout(function() {
+			resolve(new Response("Hi, Barret Lee" + new Date()));
+		}, 1000);
+	}));
 });
 
 console.info("workker.js");
