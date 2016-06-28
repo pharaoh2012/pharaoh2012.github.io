a7220e8a471919e82ca287b2884df5efbe6e0dd6
raoming <raoming@dcqtech.com>
2016-06-28 17:59:11 +0800

    多看阅读

---
+++ android/js/多看阅读.js
@@ -0,0 +1,8 @@
+
+
+ServerTools.runPackage("com.duokan.reader");
+ServerTools.sleep(15000);
+ServerTools.click(476,94,"点击书架");  ServerTools.sleep(5000);
+ServerTools.click(587,278,"签到");  ServerTools.sleep(10000);
+
+ServerTools.exit(true);
